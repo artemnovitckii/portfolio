1c798fb0a702bb3ea967224e60789aba42df8de3
Artem Novitckii <artemnovitckii@gmail.com>
2021-09-25 17:05:59 +1200

    first commit :squirrel:
    did nav bar

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        navy: "#264653",
+        green: "#2A9D8F",
+        yellow: "#E9C46A",
+        orange: "#F4A261",
+        coral: "#E76F51",
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
